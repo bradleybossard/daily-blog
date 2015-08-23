15ab02fba0615dff833ddf60dade06aa8e8bd50e
Bradley Bossard <bradleybossard@gmail.com>
2015-08-23 21:29:54 +0000

    Updates to script for automatically creating articles.

--- scripts/new-article.sh
+++ scripts/new-article.sh
@@ -1,4 +1,5 @@
 DATE=`date +%Y-%m-%d`
 ARTICLE_PATH='./contents/articles/'$DATE
 mkdir -p $ARTICLE_PATH
-cp ./scripts/index.md $ARTICLE_PATH/
+cp ./scripts/index.md $ARTICLE_PATH
+cat ./scripts/index.md | sed "s/todays_date/$DATE/g" > $ARTICLE_PATH/index.md
